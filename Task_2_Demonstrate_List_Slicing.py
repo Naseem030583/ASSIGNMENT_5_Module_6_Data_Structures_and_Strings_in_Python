1431085abd2a82dc66a4560938bfa96ecd90d7db
Naseem Ahamad <137470997+Naseem030583@users.noreply.github.com>
2025-10-06 11:39:57 -0500

    Add files via upload

---
+++ Task_2_Demonstrate_List_Slicing.py
@@ -0,0 +1,10 @@
+# Create list from 1 to 10
+numbers = list(range(1, 11))
+
+first_five = numbers[:5]
+
+reversed_list = first_five[::-1]
+
+print(f"Original list: {numbers}")
+print(f"Extracted first five elements: {first_five}")
+print(f"Reversed extracted elements: {reversed_list}")
